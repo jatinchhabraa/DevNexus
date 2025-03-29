3b8e6b04ce9422f82a091492462922f73d106df7
jatinchhabraa <jchhabra_be22@thapar.edu>
2025-03-30 01:12:30 +0530

    created endpoints to add users to project and to get all users

--- backend/routes/project.routes.js
+++ backend/routes/project.routes.js
@@ -13,4 +13,17 @@ router.post(
 );
 
 router.get("/all", authMiddleware.authUser, projectController.getAllProject);
+
+router.put(
+  "/add-user",
+  authMiddleware.authUser,
+  body("projectId").isString().withMessage("Project ID is required"),
+  body("users")
+    .isArray({ min: 1 })
+    .withMessage("Users must be an array of strings")
+    .bail()
+    .custom((users) => users.every((user) => typeof user === "string"))
+    .withMessage("Each user must be a string"),
+  projectController.addUserToProject
+);
 export default router;
