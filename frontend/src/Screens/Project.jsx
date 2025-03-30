d5f062f54651867536ccf93d7306a0e33081e5a1
jatinchhabraa <jchhabra_be22@thapar.edu>
2025-03-30 16:10:48 +0530

    craeted the messaging UI

--- frontend/src/Screens/Project.jsx
+++ frontend/src/Screens/Project.jsx
@@ -8,12 +8,38 @@ const Project = () => {
 
   return (
     <main className="h-screen w-screen flex ">
-      <section className="left h-full min-w-60 bg-red-300">
-        <header className="flex justify-end p-2 px-4 w-full bg-slate-200">
-          <button className="p-2 ">
+      <section className="left flex flex-col h-full min-w-72 bg-slate-300 ">
+        <header className="flex justify-end p-2 px-4 w-full bg-slate-100">
+          <button className="p-2  ">
             <i className="ri-group-fill"></i>
           </button>
         </header>
+
+        <div className="conversation-area flex-grow flex flex-col">
+          <div className="p-1 message-box flex-grow flex flex-col gap-1">
+            <div className="message max-w-56 flex flex-col p-2 bg-slate-50 w-fit rounded-md">
+              <small className="opacity-65 text-xs">example@gmail.com</small>
+              <p className="text-sm">
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                Tempore, eligendi?
+              </p>
+            </div>
+            <div className="ml-auto max-w-56 message flex flex-col p-2 bg-slate-50 w-fit rounded-md">
+              <small className="opacity-65 text-xs">example@gmail.com</small>
+              <p className="text-sm">Lorem ipsum dolor sit amet.</p>
+            </div>
+          </div>
+          <div className="inputField w-full flex  ">
+            <input
+              className="p-2 px-4 border-none outline-none bg-white"
+              type="text"
+              placeholder="Enter message"
+            />
+            <button className="flex-grow px-3">
+              <i className="ri-send-plane-fill"></i>
+            </button>
+          </div>
+        </div>
       </section>
     </main>
   );
