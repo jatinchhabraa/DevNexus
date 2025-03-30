ddb989b93aef19762217a1149ab27bb4f48ef060
jatinchhabraa <jchhabra_be22@thapar.edu>
2025-03-30 14:36:19 +0530

    improved project UI

--- frontend/src/Screens/Project.jsx
+++ frontend/src/Screens/Project.jsx
@@ -6,7 +6,17 @@ const Project = () => {
 
   console.log(location.state);
 
-  return <div>Project</div>;
+  return (
+    <main className="h-screen w-screen flex ">
+      <section className="left h-full min-w-60 bg-red-300">
+        <header className="flex justify-end p-2 px-4 w-full bg-slate-200">
+          <button className="p-2 ">
+            <i className="ri-group-fill"></i>
+          </button>
+        </header>
+      </section>
+    </main>
+  );
 };
 
 export default Project;
