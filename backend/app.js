9f754fdcacdec73887af12c850b794599507da29
jatinchhabraa <jchhabra_be22@thapar.edu>
2025-07-07 02:11:04 +0530

    Integerated web containers in the project

--- backend/app.js
+++ backend/app.js
@@ -3,9 +3,9 @@ import morgan from "morgan";
 import connect from "./db/db.js";
 import userRoutes from "./routes/user.routes.js";
 import projectRoutes from "./routes/project.routes.js";
+import aiRoutes from "./routes/ai.routes.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-
 connect();
 
 const app = express();
@@ -15,10 +15,11 @@ app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+
 app.use("/users", userRoutes);
 app.use("/projects", projectRoutes);
+app.use("/ai", aiRoutes);
 
-// dummy route
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
